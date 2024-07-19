5d07f643ad8dcdd00e3cc9baa88b85ce041c4abd
Lukasz Ozimek <lukaszozimek@Lukaszs-MacBook-Air.local>
2024-07-19 18:50:11 -0400

    Initial commit for Rust. Added hello_world and hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646.d
@@ -0,0 +1,5 @@
+/Users/lukaszozimek/Rust_Projects/Rust_Projects/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646: src/main.rs
+
+/Users/lukaszozimek/Rust_Projects/Rust_Projects/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/lukaszozimek/Rust_Projects/Rust_Projects/hello_cargo/target/debug/hello_cargo: /Users/lukaszozimek/Rust_Projects/Rust_Projects/hello_cargo/src/main.rs
